2d5a1da4f1e712773997f3cbac382dd945e61058
youngmoon525 <77032646+youngmoon525@users.noreply.github.com>
2021-10-26 17:52:31 +0900

    20211026 업로드
    DAO - SELECT

--- WorkSpace/17.ExternalAPI_Jar/src/Ex01_Api_Jar.java
+++ WorkSpace/17.ExternalAPI_Jar/src/Ex01_Api_Jar.java
@@ -1,11 +1,24 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 //DAO <- 
 public class Ex01_Api_Jar {
 	public static void main(String[] args) {
-		Ex01_DAO dao = null ;
-		dao.dbClose();
+		Ex01_DAO dao = new Ex01_DAO();
+		ArrayList<Ex01_DTO> list =  dao.memberSelect();
+		for (Ex01_DTO dto : list) {
+			System.out.println(dto.getName());
+		}
+		for(int i=0; i<list.size(); i++) {
+			System.out.println(list.get(i).getName());
+		}
+		
+		Ex01_HRDAO daoh = new Ex01_HRDAO();
+		ArrayList<HrDTO> list2 = daoh.memberSelect();
+		for (HrDTO dto : list2) {
+			System.out.println(dto.getFirst_name());
+		}
 		//dao.getConn();
 		///dao.dbClose();
 		
--- WorkSpace/17.ExternalAPI_Jar/src/Ex01_DAO.java
+++ WorkSpace/17.ExternalAPI_Jar/src/Ex01_DAO.java
@@ -3,6 +3,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 //Data Access Object
 //Java Database Connectivity
@@ -16,7 +17,7 @@ public class Ex01_DAO { // 실제 DB와 연동하여 요구사항 처리 후 리
 
 	// Connection을 리턴하는 메소드를 만들어야한다.
 	// 리턴타입이 있는 (void가 아닌 메소드)
-
+	//conn = Connection ;
 	// DB접속 메소드
 	public Connection getConn() {
 		// url(아이피정보 , 포트정보 , 오라클 버전 정보)
@@ -30,6 +31,7 @@ public class Ex01_DAO { // 실제 DB와 연동하여 요구사항 처리 후 리
 		String password = "0000";
 
 		try {
+			//oracle.jdbc.driver.OracleDriver
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			conn = DriverManager.getConnection(url, user, password);
 			//임시로 테스트할 코딩을 추가 연결테스트 완료후 삭제예정
@@ -56,12 +58,75 @@ public class Ex01_DAO { // 실제 DB와 연동하여 요구사항 처리 후 리
 	//종료순서(역순) : 결과 객체 비우기(닫기) , 전송객체 닫기 , 접속객체 닫기
 	public void dbClose() {
 		try {
-			rs.close();
-			ps.close();
-			conn.close();
+			//NullPointerException
+			//를 가장 쉽게 회피(예외처리)하는 방법.
+			if(rs != null) {rs.close();}
+			if(ps != null) {ps.close();}
+			if(conn != null) {conn.close();}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	public void connTest() {
+		//전송시 필요한것 (sql문) == String
+		//전송객체를 접속객체를 이용해서 초기화 시켜야함.
+		conn = getConn();//DB접속이 가능하게 Connection conn 객체를 초기화 시키기.
+		String userData = "kymData";
+		
+		//String sql = " select '"   + userData + "' data from dual ";
+		//구버전 방법 어떤 파라메터를 오라클까지 전송하기에 매우 불편하다.↑
+		String sql = " select ? as data1 , ? as data2 from dual ";
+		//select 'kymData' as data1, ? as data2 from dual
+		System.out.println(sql);
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, userData);
+			ps.setString(2, "test");
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				//data를 받는 방법 ( dataGethering )
+				//int num = rs.getInt("num");
+				String data = rs.getString("data1");
+				//?
+				System.out.println(data + "데이터가 있습니다."+rs.getString("data2"));
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("connTest()에서 에러 발생!");
+		}finally {
+			// 1.접속 2.전송객체를 통해 sql문 보냄 3.결과객체에 전송객체에 결과를 담음.
+			// 4.연결을 해제한다.
+			dbClose();
+		}
+	}
+	
+	public ArrayList<Ex01_DTO> memberSelect() {
+		ArrayList<Ex01_DTO> list = new ArrayList<>();
+		conn = getConn();//접속 객체를 메소드를 이용해서 초기화.
+		String sql = " select * from tblmember ";
+		try {
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+			//전송객체가 쿼리를 실행하고 그결과를 rs에 담음
+			while(rs.next()) {
+				Ex01_DTO dto = new Ex01_DTO(rs.getInt("num")
+								, rs.getString("name")
+								, rs.getInt("age")
+								, rs.getString("addr")
+								, rs.getString("tel")
+								);
+				//list.add(e)
+				list.add(dto);
+				//Ex01_DTO dto = new Ex01_DTO(1, "1", 2, "3", "4");
+				//list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			dbClose();
+		}
+		return list;
 	}
 }
